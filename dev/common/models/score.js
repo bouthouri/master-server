3441ccce69d9ac64c433f43d7b2c4fb0de0fdde7
Oussama Bouthouri <oussama@bouthouri.com>
2020-03-22 07:25:14 +0100

    added custom methode to post score

--- dev/common/models/score.js
+++ dev/common/models/score.js
@@ -1,5 +1,20 @@
 'use strict';
 
-module.exports = function(Score) {
+module.exports = function (Score) {
 
+    Score.register = function (name, score, cb) {
+        Score.create({ name: name, score: score }, function (err, res) {
+            if (err) return cb(err)
+            Score.find({
+                "limit": 10,
+                "order": "score desc",
+                "fields": {
+                    "id": false
+                }
+            }, function (err, res) {
+                if (err) return cb(err)
+                cb(null, res);
+            })
+        });
+    }
 };
