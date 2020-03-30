8741d988f3ea6cdcc800dd5dfba8c8cb69491591
Oussama Bouthouri <oussama@bouthouri.com>
2020-03-30 09:56:40 +0100

    implement function on the profil.js with correct method name

--- dev/common/models/profil.js
+++ dev/common/models/profil.js
@@ -1,6 +1,7 @@
 "use strict";
 
 module.exports = function(Profil) {
+  // a methode for email reset
   Profil.on("resetPasswordRequest", function(info, cb) {
     console.log(info);
     Profil.app.models.Mail.send(
@@ -16,4 +17,43 @@ module.exports = function(Profil) {
       }
     );
   });
+
+  // methode to register users's scores
+  Profil.addScore = function(name, score, cb) {
+    Profil.create({ name: name, score: score }, function(err, res) {
+      if (err) return cb(err);
+      Profil.find(
+        {
+          limit: 10,
+          order: "score desc",
+          fields: {
+            id: false
+          }
+        },
+        function(err, res) {
+          if (err) return cb(err);
+          cb(null, res);
+        }
+      );
+    });
+  };
+  // methode to register guests scores
+  Profil.addScoreGuest = function(name, score, cb) {
+    Profil.create({ name: name, score: score }, function(err, res) {
+      if (err) return cb(err);
+      Profil.find(
+        {
+          limit: 10,
+          order: "score desc",
+          fields: {
+            id: false
+          }
+        },
+        function(err, res) {
+          if (err) return cb(err);
+          cb(null, res);
+        }
+      );
+    });
+  };
 };
--- dev/common/models/score.js
+++ dev/common/models/score.js
@@ -1,20 +1,3 @@
-'use strict';
+"use strict";
 
-module.exports = function (Score) {
-
-    Score.register = function (name, score, cb) {
-        Score.create({ name: name, score: score }, function (err, res) {
-            if (err) return cb(err)
-            Score.find({
-                "limit": 10,
-                "order": "score desc",
-                "fields": {
-                    "id": false
-                }
-            }, function (err, res) {
-                if (err) return cb(err)
-                cb(null, res);
-            })
-        });
-    }
-};
+module.exports = function(Score) {};
